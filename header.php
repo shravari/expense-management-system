53d2885e2328edec744bb051e339ce6d1522abec
shravari <shravarip@gmail.com>
2020-01-03 16:51:20 +0530

    create welcome (index) page

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
   <!-- Custom fonts for this template-->
   <link href="fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
   <link href="css/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 
